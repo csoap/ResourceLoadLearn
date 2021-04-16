3956120c78a39b6c80176613a46ea39800d52e1e
caizunzao <csoap994@gmail.com>
2021-04-17 00:03:23 +0800

    init

---
+++ Assets/Editor/BundleEditor.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BundleEditor{
+    public static string ABCONFIGPATH = "Assets/Editor/ABConfig.asset";
+    //key ab包名， value是路径， 所有文件夹ab包dic 
+    public static Dictionary<string, string> m_AllFileDir = new Dictionary<string, string>();
+    //过滤List
+    public static List<string> m_AllFileAB = new List<string>();
+    //单个prefab的ab包
+    public static Dictionary<string, List<string>> m_AllPrefabDir = new Dictionary<string, List<string>>();
+    [MenuItem("Tools/打包")]
+    public static void Builder()
+    {
+        m_AllFileDir.Clear();
+        m_AllFileAB.Clear();
+        ABConfig abConfig = AssetDatabase.LoadAssetAtPath<ABConfig>(ABCONFIGPATH);
+        foreach (ABConfig.FileDirABName fileDir in abConfig.m_AllFileDirAB)
+        {
+            //Debug.Log(fileDir.ABName);
+            //Debug.Log(fileDir.Path);
+            if (m_AllFileDir.ContainsKey(fileDir.ABName))
+            {
+                Debug.LogError("AB包配置名字重复，请检查");
+            }
+            else
+            {
+                m_AllFileDir.Add(fileDir.ABName, fileDir.Path);
+                m_AllFileAB.Add(fileDir.Path);
+            }
+        }
+        string[] allStr = AssetDatabase.FindAssets("t:Prefab", abConfig.m_AllPrefabPath.ToArray()); //GUID 数组
+        for (int i = 0; i < allStr.Length; i++)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(allStr[i]);
+            EditorUtility.DisplayProgressBar("查找Prefab", "Prefab: " + path, i * 1.0f / allStr.Length); // 进度条
+            if (!ContainAllFileAB(path))
+            {
+                GameObject obj = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                string[] allDepend = AssetDatabase.GetDependencies(path); // prefab对应依赖资源路径数组
+                List<string> allDependPath = new List<string>(); // 这些依赖资源以prefab名字打ab包名字
+                for (int j = 0; j < allDepend.Length; j++)
+                {
+                    //过滤cs文件
+                    if (!ContainAllFileAB(allDepend[j]) && !allDepend[j].EndsWith(".cs"))
+                    {
+                        m_AllFileAB.Add(allDepend[j]);
+                        allDependPath.Add(allDepend[j]);
+                    }
+                }
+                if (m_AllPrefabDir.ContainsKey(obj.name))
+                {
+                    Debug.LogError("存在相同名字的prefab ：" + obj.name);
+                }
+                else
+                {
+                    m_AllPrefabDir.Add(obj.name, allDependPath);
+                }
+            }
+        }
+        //设置AB包名字
+        foreach (string name in m_AllFileDir.Keys)
+        {
+            SetABName(name, m_AllFileDir[name]);
+        }
+
+        foreach (string name in m_AllPrefabDir.Keys)
+        {
+            SetABName(name, m_AllPrefabDir[name]);
+        }
+
+        EditorUtility.ClearProgressBar(); //清掉进度条
+        //BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, BuildAssetBundleOptions.ChunkBasedCompression, EditorUserBuildSettings.activeBuildTarget);
+        //AssetDatabase.Refresh();
+    }
+    // 设置AB名字
+    static void SetABName(string name, string path)
+    {
+        AssetImporter assetImporter = AssetImporter.GetAtPath(path);
+        if (assetImporter == null)
+        {
+            Debug.LogError("不存在此路径文件: " + path);
+        }
+        else
+        {
+            assetImporter.assetBundleName = name;
+        }
+    }
+
+    static void SetABName(string name, List<string> path)
+    {
+        for (int i = 0; i < path.Count; i++)
+        {
+            SetABName(name, path[i]);
+        }
+    }
+
+    static bool ContainAllFileAB(string path)
+    {
+        for (int i = 0; i < m_AllFileAB.Count; i++)
+        {
+            if (path == m_AllFileAB[i] || path.Contains(m_AllFileAB[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
